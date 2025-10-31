f295907e1bfc49bda00a0f8332b35fa60b864c72
Navneet Aman <contact@navneetaman.com>
2025-10-31 13:06:24 +0530

    Allow mp loss for non heal use. If not enabled all in config.

--- NoMpLoss/Plugin.cs
+++ NoMpLoss/Plugin.cs
@@ -9,7 +9,8 @@ namespace NoMpLoss;
 public class Plugin : BaseUnityPlugin
 {
     internal static ManualLogSource logger;
-    private ConfigEntry<bool> config;
+    private static ConfigEntry<bool> enableMod;
+    private static ConfigEntry<bool> enableAll;
 
     private void Awake()
     {
@@ -17,22 +18,36 @@ public class Plugin : BaseUnityPlugin
         logger = base.Logger;
         logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
-        config = Config.Bind("General",      // The section under which the option is shown
-                    "enable",  // The key of the configuration option in the configuration file
-                    true, // The default value
-                    "Enable mod."
-                );
-        if (config.Value)
+        enableMod = Config.Bind("General",      // The section under which the option is shown
+            "enable",  // The key of the configuration option in the configuration file
+            true, // The default value
+            "Enable mod."
+        );
+        enableAll = Config.Bind("General",      // The section under which the option is shown
+            "all",  // The key of the configuration option in the configuration file
+            false, // The default value
+            "Enable for non-heal mp usage."
+        );
+        if (enableMod.Value)
         {
             Harmony.CreateAndPatchAll(typeof(Plugin), null);
         }
 
     }
-    
+
     [HarmonyPatch(typeof(PlayerData), nameof(PlayerData.TakeMP))]
     private static void Prefix(ref int amount)
     {
-        amount = 0;
+        if (enableAll.Value)
+        {
+            amount = 0;
+        }
+    }
+    [HarmonyPatch(typeof(HeroController), nameof(HeroController.TakeMP))]
+    private static void Prefix(HeroController __instance, ref int amount)
+    {
+        if (Traverse.Create(__instance).Field<bool>("drainMP").Value && amount == 1)
+            amount = 0;
     }
 
 
